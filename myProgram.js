f44fa8c4abeaf24238fe97f57b58b38123923b2c
mariesbucket <mariereveronc98@gmail.com>
2023-05-30 20:52:30 -0300

    Add function that gives Computer a random choice

--- myProgram.js
+++ myProgram.js
@@ -1,4 +1,15 @@
-
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    
+    if (randomNumber === 1) {
+        return 'Rock';
+    } else if (randomNumber === 2) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    };
+    
+};
 
 
 
